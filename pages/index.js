24b5bc30e0916143eacd65823a9a4f8c6b05a6b5
angadxsingh <106704008+angadxsingh@users.noreply.github.com>
2022-06-02 22:10:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <div>
     <h1>demowebsite</h1>
     <p>Written in the early 1500s, this is the classic guide on how to acquire and maintain political power (even if those methods are sometimes unsavory) — a so-called “primer for princes.” Its precepts are direct, if not disturbingly cold in their formulaic pragmatism. It asks the classic question: “Do the ends justify the means?” A worthy read for any man wishing to better understand the motivations and actions that tend to rule modern politics.</p>
-    <a href="https://www.youtube.com/" target="_blank">To YouTube</a>
-    <a href="https://www.google.co.in" target="_blank">To Google India</a>
+    <a href="https://www.youtube.com/">To YouTube</a>
+    <a href="https://www.google.co.in">To Google India</a>
   </div>
   )
 }
